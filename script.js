97940193cf1183a741497d1823425b2b920d656e
August-Roosi <augustroosi@gmail.com>
2023-11-26 22:01:56 +0200

    Added like button, reset button and common footer.

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ window.onload = function() {
                 let image      = document.createElement("img")
                 let profileBox = document.createElement("div")
                 let link       = document.createElement("a")
-                let icon       = document.createElement("img")                
+                let icon       = document.createElement("img")         
+                
+                let likeTray   = document.createElement("div") 
+                let likeButton = document.createElement("button") 
+                let likeCount  = 0
 
                 header.className     = "postHeader"
                 postBox.className    = "posts"
@@ -26,6 +30,8 @@ window.onload = function() {
                 profileBox.className = "postLogoImage"
                 link.className       = ""
                 icon.className       = "postLogoImage"
+                likeButton.className = "likeButton"
+                likeButton.innerText = "Like"
 
 
                 console.log(header)
@@ -41,6 +47,10 @@ window.onload = function() {
                     image.src = "data:image/jpeg;charset=utf-8;base64," + obj.postimage 
                 }
                 
+                likeButton.addEventListener("click", function () {
+                    likeCount++;
+                    header.querySelector(".likeCount").innerText = likeCount;
+                });
 
                 link.appendChild(icon)
                 profileBox.appendChild(link)
@@ -49,6 +59,9 @@ window.onload = function() {
                 postBox.appendChild(image)
                 mainBox.appendChild(postBox)
                 postBox.innerHTML = postBox.innerHTML + obj.posttext
+                likeTray.appendChild(likeButton)
+                likeTray.innerHTML += `<span class="likeCount">${likeCount}</span>`
+
   
             }
         }) 
@@ -57,6 +70,17 @@ window.onload = function() {
             console.log(err)
             box.innerHTML = "error"
         })
+
+        let resetButton = document.createElement("button");
+        resetButton.className = "resetButton";
+        resetButton.innerText = "Reset Likes";
+        mainBox.appendChild(resetButton);
+
+        resetButton.addEventListener("click", function () {
+            document.querySelectorAll(".likeCount").forEach((count) => {
+                count.innerText = 0;
+            });
+        });
 
         const profilePhoto = document.querySelector("#logo img");
         const userDropdown = document.querySelector("#userDropdown");
--- vueveebisats/src/components/PostComponent.vue
+++ vueveebisats/src/components/PostComponent.vue
@@ -1,50 +1,51 @@
 <template>
- 
   <div id="mainPage">
     <div class="posts" v-for="post in postList" :key="post.id">
-        <div class="postHeader">                
-            <div class="postLogoImage">
-                <a>   
-                    <img :src="post.userimage" alt="Profile Pic" class="postLogoImage">  
-                    
-                </a>
-            </div>     
-            {{post.postcreatedate}}
+      <div class="postHeader">
+        <div class="postLogoImage">
+          <a>
+            <img :src="post.userimage" alt="Profile Pic" class="postLogoImage" />
+          </a>
         </div>
-        <img :src="post.postimage"   alt="Post 1 image" class="postImage">
-        {{post.posttext}}   
+        {{ post.postcreatedate }}
+      </div>
+      <img :src="post.postimage" alt="Post 1 image" class="postImage" />
+      {{ post.posttext }}
+      <div class="likeSection">
+        <button v-on:click="Increaselikes(post.id)" class="likeButton">       
+               <img src="../assets/like.png"  class="likeIcon" >
+        </button>
+        <span class="likeCount">{{ post.likecount }} likes</span>
+      </div>
     </div>
   </div>
+</template>
 
+<script>
+export default {
+  name: "PostComponent",
+  data() {
+    return {};
+  },
+  computed: {
+    postList() {
+      return this.$store.state.postList;
+    },
+  },
+  methods: {
+    Increaselikes(postId) {
+      this.$store.dispatch("IncreaseLikesAct", postId);
+    },
+  },
+};
+</script>
 
-
-    </template>
-    <script>
-    export default {
-    name: "PostComponent",
-    data: function() {
-      
-    return {
-    
-    }},
-    computed: {
-        postList(){
-          return this.$store.state.postList
-      }
-    }
-    }
-    </script>
-    
-
-    <style scoped>
+<style scoped>
 :root {
   font-family: 'Courier Prime', monospace;
 }
-/* Navigation menu/navigation bar */
-/* Logo link frontpage */
 
-/* logo div */
-#logo{    
+#logo {
   display: flex;
   padding-left: 20px;
   padding-top: 5px;
@@ -52,46 +53,67 @@
   padding-right: 20px;
   width: 50px;
   height: 50px;
-  }
+}
+
+.posts {
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  margin-left: 100px;
+  margin-top: 25px;
+  margin-bottom: 25px;
+  margin-right: 100px;
+  background-color: lightgray;
+}
+
+.postImage {
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+
+.postHeader {
+  overflow: hidden;
+  align-items: center;
+  text-align: right;
+}
+
+.postLogoImage {
+  float: left;
+  margin-left: auto;
+  margin-right: auto;
+  width: 43px;
+  height: 47px;
+}
 
 
-/* posts div */
-.posts{      
-    border-radius: 15px;
-    display: flex;
-    flex-direction: column;
-    padding: 10px;
-    margin-left: 100px;
-    margin-top: 25px;
-    margin-bottom: 25px;
-    margin-right: 100px;
-    background-color: lightgray;
-  }
+.likeSection {
+  display: flex;
+  justify-content: space-between;
+}
 
-  .postImage {
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
-  }
+.likeButton {
+  background: none;
+  border: none;
+  cursor: pointer;
+  width: 10%;
+  height: 100%;
+  padding: 0;
+  margin-right: 0;
+  
+}
 
-       /* image div */
+.likeCount {
+  padding-top: 4.5%;
+  margin-right: 0; 
+}
 
-  .postHeader {   
-    overflow: hidden;
-    align-items: center;
-    text-align: right;
-  }
-  .postLogoImage {
-    float: left;
-    margin-left: auto;
-    margin-right: auto;
-    width: 43px;
-    height: 47px;
-  }
-  .postHeader i{   
-    float: right;
-    position: relative;
-    text-align: right;
-  }
+.likeIcon {
+  width: 4em;
+  margin: 0%;
+  padding: 0%;
+  height: 100%;
+}
 
-    </style>
+</style>
